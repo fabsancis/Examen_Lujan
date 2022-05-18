3ad7540c324c6f47df27efde8c7d44389bb243ca
fabsancis <fabsfabs786@gmail.com>
2022-05-18 03:42:09 -0300

    actualización

---
+++ ejercicio_1/ejercicio_1.dpr
@@ -0,0 +1,17 @@
+program ejercicio_1;
+
+uses
+  Vcl.Forms,
+  Form in 'Form.pas' {Form1},
+  Funciones in 'Funciones.pas',
+  ListPointer in 'ListPointer.pas',
+  Tipos in 'Tipos.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
